1512dbbdec6942994d2121c4bdbd4adc4c256a26
Timothy Duong <34667840+mrtimothyduong@users.noreply.github.com>
2025-11-21 08:43:24 +1100

    updated firefox extension files

---
+++ firefox/README.md
@@ -0,0 +1,14 @@
+# Shapr3D Export to STL for free!
+
+## This is a Firefox extension that allows the download of high-resolution models out of the web preview. Huge thanks to three.js for being awesome.
+
+
+To install:
+1. Download these files to your local machine
+2. Open about:debugging#/runtime/this-firefox
+3. Click "Load Temporary Add-on..."
+4. Select the manifest.json file from the folder you just downloaded
+
+To use:
+
+With the extension active, visit any Shapr3D preview link. An "Export STL" button should appear in the bottom right corner.
---
+++ firefox/injected.js
@@ -0,0 +1,39 @@
+const originalFetch = window.fetch;
+
+// Override the window.fetch with our custom function
+// This is so we can grab the URL of the raw gltf file
+
+window.fetch = async function (...args) {
+
+  const [url, options] = args;
+
+
+  if (url.method == "GET"){
+    if (url.url.startsWith("blob:https://app.shapr3d.com/")){
+
+      //found the blob url
+      window.MODEL_GLTF_URL = url.url
+      stl_ready()
+
+    }
+  }
+
+
+  // 4) Call the original fetch
+  const response = await originalFetch(...args);
+
+  return response;
+};
+
+
+
+window.stl_ready = async function(){
+
+    var url = "https://static.candela.io/three/stlmaker.js"
+
+    const script = document.createElement('script');
+    script.src = url;
+    script.type = "module"
+    document.head.appendChild(script);
+
+}
